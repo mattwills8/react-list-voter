a066bf34418bddc0199bb99bd07266ba2816ea1e
mattwills8 <matt_wills8@hotmail.co.uk>
2017-10-11 23:28:44 -0500

    add initialisation of list voter item post type on activation

--- includes/class-reactjs-list-voter-activator.php
+++ includes/class-reactjs-list-voter-activator.php
@@ -31,6 +31,41 @@ class Reactjs_List_Voter_Activator {
 	 */
 	public static function activate() {
 
+		add_action( 'init', array('this','cptui_register_my_cpts_list_voter_list_item') );
+	}
+
+	public function cptui_register_my_cpts_list_voter_list_item() {
+
+		/**
+		 * Post Type: List Voter List Items.
+		 */
+
+		$labels = array(
+			"name" => __( "List Voter List Items", "storefront" ),
+			"singular_name" => __( "List Voter List Item", "storefront" ),
+		);
+
+		$args = array(
+			"label" => __( "List Voter List Items", "storefront" ),
+			"labels" => $labels,
+			"description" => "",
+			"public" => true,
+			"publicly_queryable" => true,
+			"show_ui" => true,
+			"show_in_rest" => true,
+			"rest_base" => "list-voter-list-item",
+			"has_archive" => false,
+			"show_in_menu" => true,
+			"exclude_from_search" => false,
+			"capability_type" => "post",
+			"map_meta_cap" => true,
+			"hierarchical" => false,
+			"rewrite" => array( "slug" => "list_voter_list_item", "with_front" => true ),
+			"query_var" => true,
+			"supports" => array( "title", "editor", "thumbnail" ),
+		);
+
+		register_post_type( "list_voter_list_item", $args );
 	}
 
 }
