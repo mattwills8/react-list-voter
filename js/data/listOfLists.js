1b38fabc97adf05ceec515db0b7de99f05e1cc3c
mattwills8 <matt_wills8@hotmail.co.uk>
2017-10-11 23:47:49 -0500

    add list item post id placeholder, prepare for adding midware and api

--- js/data/listOfLists.js
+++ js/data/listOfLists.js
@@ -2,26 +2,26 @@ var listOfLists = [
   { name: 'activities',
     id: 0,
     list: [
-      { id: 0, values: { name:'films', img_url:''}, votes:5 },
-      { id: 1, values: { name:'books', img_url:''}, votes:4 },
-      { id: 2, values: { name:'courses', img_url:''}, votes:3 },
-      { id: 3, values: { name:'walking', img_url:''}, votes:2 }
+      { id: 0, values: { name:'films', postID:'', postContent: {} , postContent: {} }, votes:5 },
+      { id: 1, values: { name:'books', postID:'', postContent: {} }, votes:4 },
+      { id: 2, values: { name:'courses', postID:'', postContent: {} }, votes:3 },
+      { id: 3, values: { name:'walking', postID:'', postContent: {} }, votes:2 }
   ]},
   { name: 'foods',
     id: 1,
     list: [
-    { id: 0, values: { name:'thai', img_url:''}, votes:10 },
-    { id: 1, values: { name:'indian', img_url:''}, votes:4 },
-    { id: 2, values: { name:'greek', img_url:''}, votes:1 },
-    { id: 3, values: { name:'italian', img_url:''}, votes:0 }
+    { id: 0, values: { name:'thai', postID:'', postContent: {} }, votes:10 },
+    { id: 1, values: { name:'indian', postID:'', postContent: {} }, votes:4 },
+    { id: 2, values: { name:'greek', postID:'', postContent: {} }, votes:1 },
+    { id: 3, values: { name:'italian', postID:'', postContent: {} }, votes:0 }
   ]},
   { name: 'sports',
     id: 2,
     list: [
-    { id: 0, values: { name:'climbing', img_url:''}, votes:100 },
-    { id: 1, values: { name:'surfing', img_url:''}, votes:100 },
-    { id: 2, values: { name:'football', img_url:''}, votes:1 },
-    { id: 3, values: { name:'croquet', img_url:''}, votes:0 }
+    { id: 0, values: { name:'climbing', postID:'', postContent: {} }, votes:100 },
+    { id: 1, values: { name:'surfing', postID:'', postContent: {} }, votes:100 },
+    { id: 2, values: { name:'football', postID:'', postContent: {} }, votes:1 },
+    { id: 3, values: { name:'croquet', postID:'', postContent: {} }, votes:0 }
   ]}
 ];
 
